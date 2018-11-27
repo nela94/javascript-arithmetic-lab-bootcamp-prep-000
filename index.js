ddf3df6df6b999e589ab93a38f0e574f4fbd5748
Manuela  Sanchez <m.sanchez947@hotmail.com>
2018-11-27 21:18:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var a, b, n 
 function add(a, b) {
   return a += b
 }
@@ -9,4 +10,10 @@ function multiply(a,b) {
 }
 function divide(a,b) {
   return a /= b
+}
+function inc(n) {
+  return n ++
+}
+function dec(n) {
+  return n --
 }
